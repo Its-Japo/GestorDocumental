a6a6f7c00126739f8e5ff9ad4c0e36cc190819ee
Japo <pue22296@uvg.edu.gt>
2024-04-12 00:06:57 -0600

    Creada la pantalla de inicio de sesión

--- Proyecto/client/GestorDocumental/src/app/app.module.ts
+++ Proyecto/client/GestorDocumental/src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { LoginComponent } from './components/login/login.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
