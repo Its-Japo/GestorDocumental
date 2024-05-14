bc583aae18f1611a22b833cc3f9239ae25ca1b86
Gabriel Paz <gpaz@bytesw.com>
2024-05-13 20:20:49 -0600

    | Client | Creacion de add-file module y agregacion de logica de rutas en routing.module

--- Proyecto/client/GestorDocumental/src/app/modules/portal/portal-routing.module.ts
+++ Proyecto/client/GestorDocumental/src/app/modules/portal/portal-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FrameComponent } from './components/frame/frame.component';
+import { HomePortalComponent } from '../../components/home-portal/home-portal.component';
 
 const routes: Routes = [
   {
@@ -8,10 +9,18 @@ const routes: Routes = [
     component: FrameComponent,
     children: [
       {
+        path: '',  // Ruta predeterminada dentro de FrameComponent
+        component: HomePortalComponent,  // Configura HomePortalComponent como predeterminado
+        pathMatch: 'full'
+      },
+      {
         path: 'search', // Esta ruta ahora cargará el módulo 'search' de manera perezosa
         loadChildren: () => import('./modules/search/search.module').then(m => m.SearchModule)
       },
-      // otras sub-rutas que también se mostrarán dentro de Frame
+      {
+        path: 'add', // Esta ruta ahora cargará el módulo 'search' de manera perezosa
+        loadChildren: () => import('./modules/add-file/add-file.module').then(m => m.AddFileModule)
+      },
     ]
   }
 ];
--- Proyecto/client/GestorDocumental/src/app/modules/portal/portal.module.ts
+++ Proyecto/client/GestorDocumental/src/app/modules/portal/portal.module.ts
@@ -4,11 +4,15 @@ import { CommonModule } from '@angular/common';
 import { PortalRoutingModule } from './portal-routing.module';
 
 import { FrameComponent } from './components/frame/frame.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
-    FrameComponent
+    FrameComponent,
+    NavbarComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
